f9aa3e20840592659f31d456afda76f9d1d0a948
hammingweight <hammingweight@gmail.com>
2012-03-27 21:07:08 +0200

    Added stub for mongo_gridfs

---
+++ src/mongo_gridfs.erl
@@ -0,0 +1,24 @@
+%% Author: carl
+%% Created: 27 Mar 2012
+%% Description: TODO: Add description to mongo_gridfs
+-module(mongo_gridfs).
+
+%%
+%% Include files
+%%
+
+%%
+%% Exported Functions
+%%
+-export([]).
+
+%%
+%% API Functions
+%%
+
+
+
+%%
+%% Local Functions
+%%
+
