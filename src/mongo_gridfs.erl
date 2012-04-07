92a69c6bf742b80d719495d76e49a5541e80cf48
hammingweight <hammingweight@gmail.com>
2012-04-07 08:54:09 +0200

    GridFS delete/1 and delete/2 functions implemented.

--- src/mongo_gridfs.erl
+++ src/mongo_gridfs.erl
@@ -1,13 +1,16 @@
 %@doc GridFS functions.
 -module(mongo_gridfs).
 
--export([delete/2]).
+-export([delete/1, delete/2]).
+
+delete(Selector) ->
+	delete(fs, Selector).
 
 delete(Coll, Selector) ->
-	FilesColl = list_to_atom(atom_to_list(Coll) ++ ".fs.files"),
-	ChunksColl = list_to_atom(atom_to_list(Coll) ++ ".fs.chunks"),
+	FilesColl = list_to_atom(atom_to_list(Coll) ++ ".files"),
+	ChunksColl = list_to_atom(atom_to_list(Coll) ++ ".chunks"),
 	Cursor = mongo:find(FilesColl, Selector, {'_id', 1}),
 	Files = mongo_cursor:rest(Cursor),
 	Ids = [Id || {'_id', Id} <- Files],
-	mongo:delete(ChunksColl, {file_id, {'$in', Ids}}),
-	mongo:delete(FilesColl, {file_id, {'$in', Ids}}).
+	mongo:delete(ChunksColl, {files_id, {'$in', Ids}}),
+	mongo:delete(FilesColl, {'_id', {'$in', Ids}}).
