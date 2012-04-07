8d3ef3c34a93ac96eec4c2866dfa9986378f3adb
hammingweight <hammingweight@gmail.com>
2012-04-07 08:27:58 +0200

    Added delete/2 function to delete files from GridFS.

--- src/mongo_gridfs.erl
+++ src/mongo_gridfs.erl
@@ -1,4 +1,13 @@
 %@doc GridFS functions.
 -module(mongo_gridfs).
 
--export([]).
+-export([delete/2]).
+
+delete(Coll, Selector) ->
+	FilesColl = list_to_atom(atom_to_list(Coll) ++ ".fs.files"),
+	ChunksColl = list_to_atom(atom_to_list(Coll) ++ ".fs.chunks"),
+	Cursor = mongo:find(FilesColl, Selector, {'_id', 1}),
+	Files = mongo_cursor:rest(Cursor),
+	Ids = [Id || {'_id', Id} <- Files],
+	mongo:delete(ChunksColl, {file_id, {'$in', Ids}}),
+	mongo:delete(FilesColl, {file_id, {'$in', Ids}}).
