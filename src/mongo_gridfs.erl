766991c4549370236ceb069f5709337f27845eff
hammingweight <hammingweight@gmail.com>
2012-04-07 19:17:47 +0200

    Added mongo_gridfs_file module.

--- src/mongo_gridfs.erl
+++ src/mongo_gridfs.erl
@@ -2,15 +2,27 @@
 -module(mongo_gridfs).
 
 -export([delete/1, delete/2]).
+-export([find_one/1, find_one/2]).
 
+-spec(delete(bson:document()) -> ok).
+%@doc Deletes files matching the selector from the fs.files and fs.chunks collections.
 delete(Selector) ->
 	delete(fs, Selector).
 
-delete(Coll, Selector) ->
-	FilesColl = list_to_atom(atom_to_list(Coll) ++ ".files"),
-	ChunksColl = list_to_atom(atom_to_list(Coll) ++ ".chunks"),
+-spec(delete(atom(), bson:document()) -> ok).
+%@doc Deletes files matching the selector from the specified bucket.
+delete(Bucket, Selector) ->
+	FilesColl = list_to_atom(atom_to_list(Bucket) ++ ".files"),
+	ChunksColl = list_to_atom(atom_to_list(Bucket) ++ ".chunks"),
 	Cursor = mongo:find(FilesColl, Selector, {'_id', 1}),
 	Files = mongo_cursor:rest(Cursor),
+	mongo_cursor:close(Cursor),
 	Ids = [Id || {'_id', Id} <- Files],
 	mongo:delete(ChunksColl, {files_id, {'$in', Ids}}),
 	mongo:delete(FilesColl, {'_id', {'$in', Ids}}).
+
+find_one(Selector) ->
+	find_one(fs, Selector).
+
+find_one(Bucket, Selector) ->
+	ok.
