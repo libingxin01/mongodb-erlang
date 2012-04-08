8caa3582d65c18c6c298b41d0dca66c2afd47412
hammingweight <hammingweight@gmail.com>
2012-04-08 16:58:10 +0200

    Some code added to read a file.

--- src/mongo_gridfs_file.erl
+++ src/mongo_gridfs_file.erl
@@ -1,6 +1,6 @@
 -module(mongo_gridfs_file).
 
--export([new/6, get_file_size/1]).
+-export([new/6, close/1, get_file_size/1, pread/3]).
 
 -behaviour(gen_server).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
@@ -11,10 +11,16 @@ new(WriteMode, ReadMode, Connection, Database, Bucket, Id) ->
 	{ok, Pid} = gen_server:start_link(?MODULE, [WriteMode, ReadMode, Connection, Database, Bucket, Id], []),
 	Pid.
 
+close(Pid) ->
+	gen_server:call(Pid, close, infinity).
+	
 -spec(get_file_size(pid()) -> {ok, Size::integer()}).
 % Get the size of the file.
 get_file_size(Pid) ->
 	gen_server:call(Pid, file_size, infinity).
+
+pread(Pid, Offset, Length) ->
+	gen_server:call(Pid, {pread, Offset, Length}, infinity).
 
 %@doc Initiates the server.
 init([WriteMode, ReadMode, Connection, Database, Bucket, Id]) ->
@@ -27,8 +33,18 @@ init([WriteMode, ReadMode, Connection, Database, Bucket, Id]) ->
 
 %@doc Responds to synchronous messages.
 handle_call(file_size, _From, State) ->
-    Reply = file_size(State),
-    {reply, Reply, State}.
+    Reply = get_attribute(State, length),
+    {reply, {ok, Reply}, State};
+handle_call(close, _From, State) ->
+	{stop, normal, ok, State};
+handle_call({pread, Offset, _Length}, _From, State) ->
+	ChunkSize = get_attribute(State, chunkSize),
+	Length = get_attribute(State, length),
+	NumChunks = (Length + ChunkSize - 1) div ChunkSize, 
+	ChunkNum = Offset div ChunkSize,
+	ChunkOffset = Offset rem ChunkSize,
+	Reply = read(State, ChunkNum, ChunkOffset, Length, NumChunks, <<>>),
+	{reply, {ok, Reply}, State}.
 
 %@doc Handles asynchronous messages.
 handle_cast(_Msg, State) ->
@@ -46,11 +62,24 @@ terminate(_Reason, _State) ->
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
-file_size(State) ->
+get_attribute(State, Attribute) ->
 	Coll = list_to_atom(atom_to_list(State#state.bucket) ++ ".files"),
-	{ok, {{length,FileSize}}} = mongo:do(State#state.write_mode, State#state.read_mode, State#state.connection, 
-							  State#state.database, 
-							  fun() ->
-									  mongo:find_one(Coll, {'_id', State#state.id}, {'_id', 0, length, 1})
-							  end),
-	{ok, FileSize}.
+	{ok, {{Attribute,Value}}} = mongo:do(State#state.write_mode, State#state.read_mode, State#state.connection,
+										 State#state.database,
+										 fun() ->
+												 mongo:find_one(Coll, {'_id', State#state.id}, {'_id', 0, Attribute, 1})
+										 end),
+	Value.
+
+read(_State, ChunkNum, _Offset, _NumToRead, NumberOfChunks, Result) when ChunkNum > NumberOfChunks ->
+	Result;
+read(_State, _ChunkNum, _Offset, NumToRead, _NumberOfChunks, Result) when NumToRead =:= 0 ->
+	Result;
+read(State, ChunkNum, Offset, _NumToRead, NumberOfChunks, Result) ->
+	Coll = list_to_atom(atom_to_list(State#state.bucket) ++ ".chunks"),
+	{ok, {{data,{bin, bin, BinData}}}} = mongo:do(State#state.write_mode, State#state.read_mode, State#state.connection,
+										 State#state.database,
+										 fun() ->
+												 mongo:find_one(Coll, {'files_id', State#state.id, n, ChunkNum}, {'_id', 0, data, 1})
+										 end),
+	BinData.
